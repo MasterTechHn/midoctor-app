90d0778efe5ae73448de9a38a0647c91bca9e927
Dennis Anariba <anariba.hn@hotmail.com>
2021-01-14 17:27:42 -0600

    index mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './modules/app-routing.module';
+import { AppMaterialModule } from './modules/app-material.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavHeroComponent } from './nav-hero/nav-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavHeroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
