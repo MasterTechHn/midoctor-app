4333f211a275b78a4f0dd192a34e56244007a489
Dennis Anariba <anariba.hn@hotmail.com>
2021-01-15 20:55:02 -0600

    footer component and index sterilized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppMaterialModule } from './modules/app-material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavHeroComponent } from './nav-hero/nav-hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavHeroComponent
+    NavHeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
