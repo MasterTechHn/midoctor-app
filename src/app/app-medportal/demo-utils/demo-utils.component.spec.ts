13d5fa30070257bec2608b37fe4b0d6e60d5f5ee
Dennis Anariba <anariba.hn@hotmail.com>
2021-02-08 22:36:50 -0600

    -comments conflicts with demo-utils.

--- src/app/app-medportal/demo-utils/demo-utils.component.spec.ts
+++ src/app/app-medportal/demo-utils/demo-utils.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CalendarHeaderComponent } from './demo-utils.component';
+// import { CalendarHeaderComponent } from './demo-utils.component';
 
-describe('DemoUtilsComponent', () => {
-  let component: CalendarHeaderComponent;
-  let fixture: ComponentFixture<CalendarHeaderComponent>;
+// describe('DemoUtilsComponent', () => {
+//   let component: CalendarHeaderComponent;
+//   let fixture: ComponentFixture<CalendarHeaderComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CalendarHeaderComponent ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ CalendarHeaderComponent ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CalendarHeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(CalendarHeaderComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
